2a5c63c12cfa6772213976b1b977de754c09c657
devmustroc <@Ismail251217>
2022-07-20 13:34:20 +0200

    1st challenge

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,8 +4,14 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: a pointer to the node to find the sibling
- * Return: 0 if node is NULL
+ * Return: no sibling, return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
